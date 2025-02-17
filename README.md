63dbfe4b16a6cb3bcd1787fc7bfeaf9cff155dfa
Ashtreighlia <ashtreighlia@gmail.com>
2025-02-17 23:52:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vanguard
+Vanguard is a custom game mode to be played with Magic: The Gathering cards. It is inspired by the Commander format, but with a fresh take on deck building and strategic play.
+
+## Getting Started
+If you're interested in trying out Vanguard, you can have a peak at the rules here.
+
+## Contributing
+As Vanguard is still in its early stages, there is plenty of room for improvement.
+If you find any issues or have any suggestions, feel free to open an issue or submit a pull request.
---
+++ js/index.js
@@ -0,0 +1,35 @@
+//window.onbeforeunload = function () {
+//  window.scrollTo(0, 0);
+//};
+
+/* Changes the opacity of the title image while scrolling down */
+var windowHeight = window.innerHeight;
+function amountscrolled() {
+  var position = window.pageYOffset;
+  newOpacity = 100 - (position / windowHeight) * 100;
+  if (newOpacity < 0) {
+    newOpacity = 0;
+  }
+  document.getElementById("parallax").style.opacity = newOpacity + "%";
+}
+
+window.addEventListener(
+  "scroll",
+  function () {
+    amountscrolled();
+  },
+  false
+);
+
+/* Scrolls down to the content if the title image is being clicked */
+async function scrollDown() {
+  for (let i = window.pageYOffset; i <= windowHeight; i += 20) {
+    if (i > windowHeight) {
+      i = windowHeight;
+    }
+    await new Promise((r) => setTimeout(r, 1));
+    window.scrollTo(0, i);
+  }
+  //very dirty fix for not scrolling to the bottom of the page
+  window.scrollTo(0, windowHeight);
+}
